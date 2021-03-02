aab373fab1d48e998424b8ffda58f97a8e12774b
dawnzju <chenhui8945@163.com>
2021-03-02 10:20:40 +0800

    changed Nage

---
+++ mesh_size/8x8/handle_output.py
@@ -0,0 +1,64 @@
+import numpy as np
+import json
+import os
+import datetime
+
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+def handle_others_output(input_json,computation_ability,output_file_name):
+    task_graph={}
+    with open(input_json,'r') as f1:
+        task_graph=json.load(f1)
+
+    for i in task_graph.keys():
+        mapto=task_graph[i]['mapto']
+        task_graph[i]['exe_time']=computation_ability[int(i)][mapto]
+    
+    with open(output_file_name,"w") as f2:
+        f2.write(json.dumps(task_graph,cls=MyEncoder))
+    print(input_json+" modified")
+
+
+def read_NoC(NoC_file_name):
+    ret=[]
+    f=open(NoC_file_name)
+    for line in f:
+        tmp=[]
+        for i in line[1:-2].split(','):
+            tmp.append(int(i))
+        ret.append(tmp)
+    return ret
+
+
+
+if __name__ == '__main__':
+    computation_ability=read_NoC('N51_H264_Mesh8x8_NoCdescription.txt')
+    
+    dir_name='./gem5_pending_results_'+str(datetime.datetime.now().month)+'.'+str(datetime.datetime.now().day)+'/'
+
+    if(os.path.exists(dir_name)==False):
+        os.makedirs(dir_name)
+
+    json_list=[]
+    for i in os.listdir('./'):
+        if(i[-5:]=='.json'):
+            json_list.append(i)
+    print("handle json files:")
+    print(json_list)
+    for i in json_list:
+        handle_others_output(i,computation_ability,output_file_name=dir_name+'test_'+i)
+    
+
+
